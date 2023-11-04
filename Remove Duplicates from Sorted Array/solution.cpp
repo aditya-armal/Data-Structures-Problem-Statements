2a0da30783ca91ea1d3a6836131415c04e8fe024
Aditya Armal <aditya.armal@srivisifai.in>
2023-11-04 21:02:23 +0530

    Remove Duplicates from Sorted Array

---
+++ Remove Duplicates from Sorted Array/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+         if (nums.empty()) {
+            return 0;
+        }
+        int uniqueCount = 1; 
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueCount] = nums[i];
+                uniqueCount++;
+            }
+        }
+        return uniqueCount;
+    }
+};
